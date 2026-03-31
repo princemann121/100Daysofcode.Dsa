c1b377e6aef39bd77bceadf43492795fb75f5af7
Fasctor01729 <luo756feng@gmail.com>
2026-03-31 08:57:16 +0530

    commit

---
+++ Day59/Q118.c
@@ -0,0 +1,85 @@
+/* Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Search value in inorder
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+// Build tree
+struct Node* buildTreeUtil(int inorder[], int postorder[],
+                           int start, int end, int* postIndex) {
+
+    if (start > end)
+        return NULL;
+
+    int rootVal = postorder[*postIndex];
+    (*postIndex)--;
+
+    struct Node* root = newNode(rootVal);
+
+    if (start == end)
+        return root;
+
+    int inIndex = search(inorder, start, end, rootVal);
+
+    // IMPORTANT: build RIGHT first
+    root->right = buildTreeUtil(inorder, postorder, inIndex + 1, end, postIndex);
+    root->left = buildTreeUtil(inorder, postorder, start, inIndex - 1, postIndex);
+
+    return root;
+}
+
+// Wrapper function
+struct Node* buildTree(int inorder[], int postorder[], int n) {
+    int postIndex = n - 1;
+    return buildTreeUtil(inorder, postorder, 0, n - 1, &postIndex);
+}
+
+// Inorder print (to verify)
+void inorderPrint(struct Node* root) {
+    if (root) {
+        inorderPrint(root->left);
+        printf("%d ", root->data);
+        inorderPrint(root->right);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int inorder[n], postorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+
+    struct Node* root = buildTree(inorder, postorder, n);
+
+    printf("Inorder of constructed tree: ");
+    inorderPrint(root);
+
+    return 0;
+}
